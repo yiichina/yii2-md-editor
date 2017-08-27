64aa320dc08f5a5722dc46ee9ca853eaec35e4da
cuileon <cuiliang@live.cn>
2017-08-27 20:31:07 +0800

    add params

--- MdEditor.php
+++ MdEditor.php
@@ -3,18 +3,38 @@
 namespace yiichina\mdeditor;
 
 use yii\Helpers\Html;
+use yii\helpers\Json;
 
 /**
  * This is just an example.
  */
 class MdEditor extends \yii\widgets\InputWidget
 {
+    public $debug = false;
+    public $codeMirror;
+    public $buttons;
+    public $disabledButtons;
+
     public function init()
     {
         parent::init();
         $view = $this->getView();
         MdEditorAsset::register($view);
-		$view->registerJs("$(\"#{$this->options['id']}\").mdEditor()");
+        $optionsArray = [];
+		if($this->debug)) {
+            $optionsArray['debug'] = true;
+        }
+        if(empty($this->codeMirror)) {
+            $optionsArray['codeMirror'] = $this->codeMirror;
+        }
+        if(empty($this->buttons)) {
+            $optionsArray['buttons'] = $this->buttons;
+        }
+        if(empty($this->codeMirror)) {
+            $optionsArray['disabledButtons'] = $this->disabledButtons;
+        }
+        $options = Json::encode($optionsArray);
+        $view->registerJs("$(\"#{$this->options['id']}\").mdEditor($options)");
     }
 
     public function run()
